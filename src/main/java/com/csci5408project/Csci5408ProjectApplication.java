6244291a5772d55f2402accd4bb1e02a2098db05
Smit Thakkar <sm928548@dal.ca>
2021-11-27 21:52:37 -0400

    Added login, registration and authentication features

--- src/main/java/com/csci5408project/Csci5408ProjectApplication.java
+++ src/main/java/com/csci5408project/Csci5408ProjectApplication.java
@@ -1,6 +1,5 @@
 package com.csci5408project;
 //Author: @Smit_Thakkar
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import java.io.File;
