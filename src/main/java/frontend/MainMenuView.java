d589fc2da19f511504e663dc252458d1bdb7e83e
Harvik Pravinbhai Kakadiya <hr911851@dal.ca>
2021-12-08 16:03:49 -0400

    ERD Generation

--- src/main/java/frontend/MainMenuView.java
+++ src/main/java/frontend/MainMenuView.java
@@ -6,6 +6,7 @@ import java.nio.file.Paths;
 //Author: @Smit_Thakkar
 import java.util.Scanner;
 
+import com.csci5408project.Erd;
 import com.csci5408project.Queries.*;
 import com.csci5408project.SqlDump;
 import com.csci5408project.validation.IdentifyQuery;
@@ -32,11 +33,8 @@ public final class MainMenuView {
 			printer.printContent("1. Execute SQL Query.");
 			printer.printContent("2. Generate SQL Dump.");
 			printer.printContent("3. Generate ERD.");
-			printer.printContent("4. Generate Data Dictionary.");
-			printer.printContent("5. View Meta Data.");
-			printer.printContent("6. Import SQL Dump.");
-			printer.printContent("7. Logout.");
-			printer.printContent("8. Transaction.");
+			printer.printContent("4. Logout.");
+			printer.printContent("5. Transaction.");
 			printer.printContent("Select an option:");
 			final String input = scanner.nextLine();
 			switch (input) {
@@ -124,21 +122,13 @@ public final class MainMenuView {
 				sqlDump.generateSqlDump();
 				break;
 			case "3":
-
+				Erd erd = new Erd();
+				erd.generateErd();
 				break;
 			case "4":
-
-				break;
-			case "5":
-
-				break;
-			case "6":
-
-				break;
-			case "7":
 				userSession.destroyUserSession();
 				return;
-			case "8":
+			case "5":
 			default:
 				break;
 			}
